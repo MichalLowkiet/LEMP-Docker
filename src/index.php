7f3d534c891d7ccbf889adedfee4026d8c3e4dd0
Michał Łowkiet <mlowkiet@gmail.com>
2022-08-04 10:38:10 +0200

    Add PHP service

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .center {
+            display: block;
+            margin: auto;
+            width: 50%;
+        }
+    </style>
+</head>
+<body>
+
+<img src="https://tech.osteel.me/images/2020/03/04/hello.gif" alt="Hello there" class="center">
+
+
+</body>
+</html>
